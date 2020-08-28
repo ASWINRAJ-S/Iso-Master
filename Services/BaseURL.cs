78d96641106d4a275df4fd86276e791302043fdc
Aswinraj <aswinnathan07@gmail.com>
2020-08-28 11:54:34 +0530

    Admin Page

--- Services/BaseURL.cs
+++ Services/BaseURL.cs
@@ -5,6 +5,8 @@
         public string isocratesWebsite = "https://www.isocrates.app/";
         public string allReports = "https://isoorgsprintrptfunctionapp.azurewebsites.net/api/Reports?code=r4Of4Hz3cE8vBJ270ctTUrWpMdQcfQp/FZZ4EtrMhiXoEqk4mJnSjA==&name=";
         public string storage = "https://isodeveastus2blob01.blob.core.windows.net/";
-        public string myGit = "https://raw.githubusercontent.com/balajiRchandran/temp/master/bello";
+        public string myGit = "https://raw.githubusercontent.com/ASWINRAJ-S/AswinJJJson/master/bello";
+        
+
     }   
 }
